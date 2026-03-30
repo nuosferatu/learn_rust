1a77fb1b0260be53bf50d3379516466f1a652171
nuosferatu <nuosferatu@163.com>
2026-03-30 22:12:04 +0800

    test fn

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,12 @@ fn add(a: i32, b: i32) -> i32 {
     a + b
 }
 
+fn sub(a: i32, b: i32) -> i32 {
+    a - b
+}
+
 fn main() {
     println!("Hello, world!");
     println!("{} {} {}", 1.2, 2.2, add(1, 2));
+    println!("{} {} {}", 1.2, 2.2, sub(1, 2));
 }
